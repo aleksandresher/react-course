c178f9fca3f98ae8178fc091517856f453284b5c
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-21 21:03:12 +0400

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
       <h1>React App</h1>
+
+      <button onClick={() => setCount((prev) => prev + 1)}>+1</button>
+      <p>{count}</p>
+      <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
     </>
   );
 }
