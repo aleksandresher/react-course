ebf6ad37362cc67d0060895b4cd8db345ce93235
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-21 23:01:53 +0400

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useReducer } from "react";
+import { useReducer, useMemo, useState } from "react";
 import "./App.css";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    default:
-      throw new Error();
-  }
-}
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "increment":
+//       return state + 1;
+//     case "decrement":
+//       return state - 1;
+//     default:
+//       throw new Error();
+//   }
+// }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, 0);
+  // const [state, dispatch] = useReducer(reducer, 0);
+  const [count, setCount] = useState(60);
+
+  {
+    /* lets you cache the result of a calculation between re-renders. */
+  }
+  const expensiveCount = useMemo(() => {
+    return count ** 2;
+  }, [count]);
   return (
     <>
-      Count: {state}
+      <p>{count}</p>
+      {/* Count: {state}
       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button> */}
     </>
   );
 }
