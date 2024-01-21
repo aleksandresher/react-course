2dd0f8286579033b76fd6f8d539eecabc1882ef6
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-21 22:20:22 +0400

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import Count from "./Count";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import CountContext from "./context/CountContext";
 
 function App() {
   const [count, setCount] = useState(0);
+  // let ref = useRef(0);
+  const inputRef = useRef(null);
+
+  // function handleClick() {
+  //   ref.current = ref.current + 1;
+  //   alert("You clicked" + ref.current + "times!");
+  // }
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
 
   useEffect(() => {
     console.log("count was changed");
@@ -12,6 +23,12 @@ function App() {
   return (
     <>
       <h1>React App</h1>
+      {/* <button onClick={handleClick}>Click me</button> */}
+      {/*this number won't update on click  because ref does not trigger re-rendering   */}
+      {/* <p>{ref.current}</p> */}
+
+      <input ref={inputRef} />
+      <button onClick={handleClick}>Focus the input</button>
       <CountContext.Provider value={count}>
         <Count />
       </CountContext.Provider>
