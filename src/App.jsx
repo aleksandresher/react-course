dd98c849d3b00e3973067687b94074ddb48495a7
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-24 23:23:39 +0400

    update object in state using spread syntax

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,44 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  function handleChange(field, value) {
+    setPerson((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  }
   return (
-    <div
-      onClick={() => {
-        alert("Button wrapper div was clicked");
-      }}
-    >
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          alert("first button was clicked");
-        }}
-      >
-        Button 1
-      </button>
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          alert("second button was clicked");
-        }}
-      >
-        Button 2
-      </button>
+    <div>
+      <label>
+        First name:
+        <input
+          value={person.firstName}
+          onChange={(e) => handleChange("firstName", e.target.value)}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={person.lastName}
+          onChange={(e) => handleChange("lastName", e.target.value)}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          value={person.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+      </label>
+      <p>
+        {person.firstName} {person.lastName} {person.email}
+      </p>
     </div>
   );
 }
